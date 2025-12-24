72046ca4537c8773bda5474bd5e5b43ac417f1cd
Elena-admin <avb.moons@gmail.com>
2025-12-24 17:35:51 +0300

    add default storage

--- scripts/homeLoading.js
+++ scripts/homeLoading.js
@@ -1,88 +1,147 @@
-const viewportWidth = window.innerWidth + 'px';
-console.log('ViewportWidth = ', viewportWidth);
+const viewportWidth = window.innerWidth + "px";
+console.log("ViewportWidth = ", viewportWidth);
 
 const root = document.documentElement;
-root.style.setProperty('--viewport-width', viewportWidth);
+root.style.setProperty("--viewport-width", viewportWidth);
 
 const pageUrl = window.location.pathname;
-let pageName = pageUrl.substring(pageUrl.lastIndexOf('/') + 1);
+let pageName = pageUrl.substring(pageUrl.lastIndexOf("/") + 1);
 console.log(pageName);
+
+//// Verify storage
+verifyStorage();
+
+function verifyTotals(totals) {
+  if (!localStorage.getItem(totals)) {
+    console.log(totals + " - none");
+    localStorage.setItem(totals, "0,0");
+  } else {
+    console.log(totals + " - here");
+  }
+}
+
+function verifyResource(resource) {
+  if (!localStorage.getItem(resource)) {
+    console.log(resource + " - none");
+    localStorage.setItem(resource, "");
+    resourceDefault(resource);
+  } else {
+    console.log(resource + " - here");
+  }
+}
+
+function resourceDefault(resource) {
+  switch (resource) {
+    case "primLang":
+      localStorage.setItem("primLang", "Russian - Русский язык");
+      break;
+    case "compLangs":
+      localStorage.setItem("compLangs", "Armenian, Greek, Finnish, Latvian");
+      break;
+    case "themesLangs":
+      localStorage.setItem(
+        "themesLangs",
+        "World sides, Seasons, Months, Weekdays"
+      );
+      break;
+    case "modeChoice":
+      localStorage.setItem("modeChoice", "Choice");
+      break;
+    default:
+      alert("Please, choose your task settings!");
+  }
+}
+
+function verifyStorage() {
+  // verify totals: totalsChoice, totalsMix, totalsLang
+  verifyTotals("totalsChoice");
+  verifyTotals("totalsMix");
+  verifyTotals("totalsLang");
+
+  // verify resources: primLang, compLangs, themesLangs, modeChoice
+  verifyResource("primLang");
+  verifyResource("compLangs");
+  verifyResource("themesLangs");
+  verifyResource("modeChoice");
+}
+
+//localStorage.removeItem("");
 
 //// Modal Results open-close
 
-const resultsButton = document.getElementById('resultsButton');
-const resultsModal = document.getElementById('resultsModal');
-const redultsClose = document.getElementById('resultsClose');
+const resultsButton = document.getElementById("resultsButton");
+const resultsModal = document.getElementById("resultsModal");
+const redultsClose = document.getElementById("resultsClose");
 
 const openModalResults = () => {
-  resultsModal.style.display = 'flex';
+  resultsModal.style.display = "flex";
 };
 
 const closeModalResults = () => {
-  resultsModal.style.display = 'none';
+  resultsModal.style.display = "none";
 };
 
 //// Results totals
 
-let enjoyChoice = document.getElementById('enjoyChoice');
-let worryChoice = document.getElementById('worryChoice');
+let enjoyChoice = document.getElementById("enjoyChoice");
+let worryChoice = document.getElementById("worryChoice");
 
-let enjoyMix = document.getElementById('enjoyMix');
-let worryMix = document.getElementById('worryMix');
+let enjoyMix = document.getElementById("enjoyMix");
+let worryMix = document.getElementById("worryMix");
 
-let enjoyLang = document.getElementById('enjoyLang');
-let worryLang = document.getElementById('worryLang');
+let enjoyLang = document.getElementById("enjoyLang");
+let worryLang = document.getElementById("worryLang");
 
-let enjoyGrand = document.getElementById('enjoyGrand');
-let worryGrand = document.getElementById('worryGrand');
+let enjoyGrand = document.getElementById("enjoyGrand");
+let worryGrand = document.getElementById("worryGrand");
 
 function grandTotals() {
-  let totalsChoice = localStorage.getItem('totalsChoice');
+  let totalsChoice = localStorage.getItem("totalsChoice");
   let strTotalsChoice;
   let numTotalsChoice;
   if (totalsChoice == null) {
-    strTotalsChoice == '0, 0';
+    strTotalsChoice == "0, 0";
     numTotalsChoice == [0, 0]; //
   } else {
-    strTotalsChoice = totalsChoice.split(',');
+    strTotalsChoice = totalsChoice.split(",");
     numTotalsChoice = strTotalsChoice.map((item) => {
       return parseInt(item.trim());
     });
   }
 
-  console.log('Totals Choice: ');
+  console.log("Totals Choice: ");
   console.log(numTotalsChoice);
 
-  let totalsMix = localStorage.getItem('totalsMix');
+  let totalsMix = localStorage.getItem("totalsMix");
   let strTotalsMix;
   let numTotalsMix;
   if (totalsMix == null) {
-    strTotalsMix = '0, 0';
+    strTotalsMix = "0, 0";
     numTotalsMix = [0, 0];
   } else {
-    strTotalsMix = totalsMix.split(',');
+    strTotalsMix = totalsMix.split(",");
     numTotalsMix = strTotalsMix.map((item) => {
       return parseInt(item.trim());
     });
   }
 
-  console.log('Totals Mix: ');
+  console.log("Totals Mix: ");
   console.log(numTotalsMix);
 
-  let totalsLang = localStorage.getItem('totalsLang');
+  let totalsLang = localStorage.getItem("totalsLang");
   let strTotalsLang;
   let numTotalsLang;
   if (totalsLang == null) {
-    strTotalsLang == '0, 0';
+    strTotalsLang == "0, 0";
     numTotalsLang == [0, 0];
   } else {
-    strTotalsLang = totalsLang.split(',');
+    strTotalsLang = totalsLang.split(",");
     numTotalsLang = strTotalsLang.map((item) => {
       return parseInt(item.trim());
     });
   }
 
-  console.log('Totals Lang: ');
+  console.log("Totals Lang: ");
   console.log(numTotalsLang);
 
   //// Results modal and report
@@ -100,9 +159,9 @@ function grandTotals() {
   let grandTotalWorry = numTotalsChoice[1] + numTotalsMix[1] + numTotalsLang[1];
   let grandTotal = grandTotalEnjoy + grandTotalWorry;
 
-  if ((pageName = 'home.html')) {
-    let grandEnjoy = document.getElementById('grandEnjoy');
-    let grandWorry = document.getElementById('grandWorry');
+  if ((pageName = "home.html")) {
+    let grandEnjoy = document.getElementById("grandEnjoy");
+    let grandWorry = document.getElementById("grandWorry");
 
     if (grandTotal === 0 || grandTotal === null) {
       grandTotal = 1;
@@ -111,31 +170,12 @@ function grandTotals() {
     let valueGrandEnjoy = Math.round((grandTotalEnjoy * 100) / grandTotal);
     let valueGrandWorry = Math.round((grandTotalWorry * 100) / grandTotal);
 
-    enjoyGrand.textContent = valueGrandEnjoy + '%';
-    worryGrand.textContent = valueGrandWorry + '%';
+    enjoyGrand.textContent = valueGrandEnjoy + "%";
+    worryGrand.textContent = valueGrandWorry + "%";
 
-    grandEnjoy.textContent = valueGrandEnjoy + '%';
-    grandWorry.textContent = valueGrandWorry + '%';
+    grandEnjoy.textContent = valueGrandEnjoy + "%";
+    grandWorry.textContent = valueGrandWorry + "%";
   }
 }
 
 grandTotals();
-
-// const divElement = document.querySelector("#resultsMain");
-// const contentHtml = divElement.innerHTML;
-// const doc = new jsPDF();
-// doc.text(contentHtml, 10, 10);
-// doc.save('testcontent.pdf');
-
-// let resultsBtn = document.querySelector("#resultsBtn");
-// resultsBtn.addEventListener('click', () => {
-//     const {jsPDF} = window.jspdf;
-//     const doc = new jsPDF();
-
-//     doc.text("Hello World!", 10, 10);
-//     doc.text("This first row.", 10, 20);
-//     doc.text("This second row.", 10, 30);
-
-//     doc.save("test-pdf.pdf");
-
-// })
--- scripts/showModals.js
+++ scripts/showModals.js
@@ -3,29 +3,32 @@
 const setButton = document.getElementById("setButton");
 const getButton = document.getElementById("getButton");
 
-const modalSettings = document.getElementById('settings');  //[0];
+const modalSettings = document.getElementById("settings"); //[0];
 
 const primLang = document.getElementById("primLang");
-const resultPrimLang = document.getElementById('resultPrim');
+const resultPrimLang = document.getElementById("resultPrim");
 const comboPrimLangsList = document.getElementById("primLangChoice");
 const comboPrimLangChoiceList = document.getElementById("primLangChoiceList");
+const inputPrim = document.getElementById("inputPrim");
 // const comboPrimLangRus = document.getElementById("primRus");
 // console.log(comboPrimLangRus);
 const compLangs = document.getElementById("compLang");
 const resultCompLangs = document.getElementById("resultComp");
 const comboCompLangsList = document.getElementById("compLangChoice");
 const comboCompLangChoiceList = document.getElementById("compLangChoiceList");
+const inputComp = document.getElementById("inputComp");
 
 const themesLangs = document.getElementById("themeList");
 const resultThemesLangs = document.getElementById("resultThemes");
 const comboThemesList = document.getElementById("themeChoice");
 const comboThemeChoiceList = document.getElementById("themeChoiceList");
+const inputThemes = document.getElementById("inputThemes");
 
 const btnOpenPrimLang = document.getElementById("btnOpenPrimLang");
 const btnOpenCompLang = document.getElementById("btnOpenCompLang");
 const btnOpenThemes = document.getElementById("btnOpenThemes");
 
-const modeButtons = document.getElementsByClassName("mode-button");  //mode buttons array
+const modeButtons = document.getElementsByClassName("mode-button"); //mode buttons array
 //const modeButtons = document.querySelectorAll('input[name="modeButton"]');
 //console.log(modeButtons);
 
@@ -38,214 +41,211 @@ const comboAppLangChoice = document.getElementById("appLangChoice");
 //console.log(modalSettings);
 
 const openModalSettings = () => {
-    primLang.value = localStorage.getItem('primLang');
-    console.log(primLang.value);
-    resultPrimLang.value = localStorage.getItem('primLang');
+  primLang.value = localStorage.getItem("primLang");
+  console.log(primLang.value);
+  resultPrimLang.value = localStorage.getItem("primLang");
 
-    compLangs.value = localStorage.getItem('compLangs');
-    console.log(compLangs.value);
-    resultCompLangs.textContent = localStorage.getItem('compLangs');
+  compLangs.value = localStorage.getItem("compLangs");
+  console.log(compLangs.value);
+  resultCompLangs.textContent = localStorage.getItem("compLangs");
 
-    themesLangs.value = localStorage.getItem('themesLangs');
-    console.log(themesLangs.value);
-    resultThemesLangs.textContent = localStorage.getItem('themesLangs');
+  themesLangs.value = localStorage.getItem("themesLangs");
+  console.log(themesLangs.value);
+  resultThemesLangs.textContent = localStorage.getItem("themesLangs");
 
-    resultMode.value = localStorage.getItem('modeChoice');
-    console.log(resultMode.value);
-    
-    modalSettings.style.display = "flex";    
+  resultMode.value = localStorage.getItem("modeChoice");
+  console.log(resultMode.value);
 
-    //console.log(modeButtons);
+  modalSettings.style.display = "flex";
 
-    let modeButtonsLength = modeButtons.length;
-    //console.log(modeButtonsLength);
+  //console.log(modeButtons);
 
-    for (i=0; i<modeButtonsLength; i++) {
-        let item = modeButtons[i].id;
-        //console.log(item);
-        if (item.includes(resultMode.value)) {
-            //console.log(item);
-            modeButtons[i].checked = true;
-            break;
-        } else {
-            const modeButtonPlain = document.getElementById("setModePlain");
-            modeButtonPlain.checked = true;
-        }
+  let modeButtonsLength = modeButtons.length;
+  //console.log(modeButtonsLength);
+
+  for (i = 0; i < modeButtonsLength; i++) {
+    let item = modeButtons[i].id;
+    //console.log(item);
+    if (item.includes(resultMode.value)) {
+      //console.log(item);
+      modeButtons[i].checked = true;
+      break;
+    } else {
+      const modeButtonPlain = document.getElementById("setModePlain");
+      modeButtonPlain.checked = true;
     }
-    
-
-
+  }
 };
 
 const closeModalSettings = () => {
-    modalSettings.style.display = "none";
-}
+  modalSettings.style.display = "none";
+};
 
 ////Combo-list open-close
 
 //const comboPrimLangsList = document.getElementById("primLangChoice");
 const openPrimLangsChoiceList = () => {
-    comboPrimLangsList.style.display = "flex";
-    comboPrimLangChoiceList.style.display = "flex"; //
-    const comboPrimLangRus = document.getElementById("primRus");    //
-    //console.log(comboPrimLangRus);  //
-    comboPrimLangRus.checked = true;  //
-    btnOpenCompLang.disabled = true;
-    btnOpenThemes.disabled = true;
-}
+  comboPrimLangsList.style.display = "flex";
+  comboPrimLangChoiceList.style.display = "flex"; //
+  const comboPrimLangRus = document.getElementById("primRus"); //
+  //console.log(comboPrimLangRus);  //
+  comboPrimLangRus.checked = true; //
+  btnOpenCompLang.disabled = true;
+  btnOpenThemes.disabled = true;
+};
 const closePrimLangsChoiceList = () => {
-    comboPrimLangsList.style.display = "none";
-    comboPrimLangChoiceList.style.display = "none"; //
-    btnOpenCompLang.disabled = false;
-    btnOpenThemes.disabled = false;
+  comboPrimLangsList.style.display = "none";
+  comboPrimLangChoiceList.style.display = "none"; //
+  btnOpenCompLang.disabled = false;
+  btnOpenThemes.disabled = false;
 
-    choosedItemRadio("primLang", "resultPrim");
+  choosedItemRadio("primLang", "resultPrim");
 
-    // const arrItems = document.getElementsByName("primLang");
-    // for (const item of arrItems) {
-    //     if (item.checked == true) {
-    //         document.getElementById("primLang").value = item.value;
-    //         break;
-    //     }
-    // }
+  // const arrItems = document.getElementsByName("primLang");
+  // for (const item of arrItems) {
+  //     if (item.checked == true) {
+  //         document.getElementById("primLang").value = item.value;
+  //         break;
+  //     }
+  // }
 
-    // arrItems.forEach(item => {
-    //     if (item.checked = true) {
-    //         document.getElementById("primLang").value = item.value;            
-    //     }
-    // });
-}
+  // arrItems.forEach(item => {
+  //     if (item.checked = true) {
+  //         document.getElementById("primLang").value = item.value;
+  //     }
+  // });
+};
 
 //const comboCompLangsList = document.getElementById("compLangChoice");
 const openCompLangsChoiceList = () => {
-    comboCompLangsList.style.display = "flex";
-    comboCompLangChoiceList.style.display = "flex"; //
-    const comboCompLangArm = document.getElementById("compArm");    //
-    const comboCompLangGre = document.getElementById("compGre");    //
-    const comboCompLangFin = document.getElementById("compFin");    //
-    const comboCompLangLav = document.getElementById("compLav");    //
-    comboCompLangArm.checked = true;
-    comboCompLangGre.checked = true;
-    comboCompLangFin.checked = true;
-    comboCompLangLav.checked = true;
-    btnOpenPrimLang.disabled = true;
-    btnOpenThemes.disabled = true;
-}
+  comboCompLangsList.style.display = "flex";
+  comboCompLangChoiceList.style.display = "flex"; //
+  const comboCompLangArm = document.getElementById("compArm"); //
+  const comboCompLangGre = document.getElementById("compGre"); //
+  const comboCompLangFin = document.getElementById("compFin"); //
+  const comboCompLangLav = document.getElementById("compLav"); //
+  comboCompLangArm.checked = true;
+  comboCompLangGre.checked = true;
+  comboCompLangFin.checked = true;
+  comboCompLangLav.checked = true;
+  btnOpenPrimLang.disabled = true;
+  btnOpenThemes.disabled = true;
+};
 const closeCompLangsChoiceList = () => {
-    comboCompLangsList.style.display = "none";
-    comboCompLangChoiceList.style.display = "none"; //
-    btnOpenPrimLang.disabled = false;
-    btnOpenThemes.disabled = false;
+  comboCompLangsList.style.display = "none";
+  comboCompLangChoiceList.style.display = "none"; //
+  btnOpenPrimLang.disabled = false;
+  btnOpenThemes.disabled = false;
 
-    choosedItemCheck("compLang", "resultComp");
-}
+  choosedItemCheck("compLang", "resultComp");
+};
 
 //const comboThemesList = document.getElementById("themeChoice");
 
 const openThemesChoiceList = () => {
-    comboThemesList.style.display = "flex";
-    comboThemeChoiceList.style.display = "flex"; //
-    const comboTheme4 = document.getElementById("theme4");    //
-    const comboTheme5 = document.getElementById("theme5");    //
-    const comboTheme6 = document.getElementById("theme6");    //
-    const comboTheme7 = document.getElementById("theme7");    //
-    comboTheme4.checked = true;
-    comboTheme5.checked = true;
-    comboTheme6.checked = true;
-    comboTheme7.checked = true;
-    btnOpenPrimLang.disabled = true;
-    btnOpenCompLang.disabled = true;
-}
+  comboThemesList.style.display = "flex";
+  comboThemeChoiceList.style.display = "flex"; //
+  const comboTheme4 = document.getElementById("theme4"); //
+  const comboTheme5 = document.getElementById("theme5"); //
+  const comboTheme6 = document.getElementById("theme6"); //
+  const comboTheme7 = document.getElementById("theme7"); //
+  comboTheme4.checked = true;
+  comboTheme5.checked = true;
+  comboTheme6.checked = true;
+  comboTheme7.checked = true;
+  btnOpenPrimLang.disabled = true;
+  btnOpenCompLang.disabled = true;
+};
 const closeThemesChoiceList = () => {
-    comboThemesList.style.display = "none";
-    comboThemeChoiceList.style.display = "none"; //
-    btnOpenPrimLang.disabled = false;
-    btnOpenCompLang.disabled = false;
+  comboThemesList.style.display = "none";
+  comboThemeChoiceList.style.display = "none"; //
+  btnOpenPrimLang.disabled = false;
+  btnOpenCompLang.disabled = false;
 
-    choosedItemCheck("themeList", "resultThemes");
-}
+  choosedItemCheck("themeList", "resultThemes");
+};
 
 const openAppLangChoiceList = () => {
-    comboAppLangChoice.style.display = "flex";
-}
+  comboAppLangChoice.style.display = "flex";
+};
 const closeAppLangChoiceList = () => {
-    comboAppLangChoice.style.display = "none";
-}
+  comboAppLangChoice.style.display = "none";
+};
 
 function choosedItemRadio(radioName, resultName) {
-    const arrItems = document.getElementsByName(radioName);
-    for (const item of arrItems) {
-        if (item.checked == true) {
-            document.getElementById(radioName).value = item.value;
-            document.getElementById(resultName).value = '';
-            document.getElementById(resultName).value += item.value + '\n';
-            break;
-        }
+  const arrItems = document.getElementsByName(radioName);
+  for (const item of arrItems) {
+    if (item.checked == true) {
+      document.getElementById(radioName).value = item.value;
+      document.getElementById(resultName).value = "";
+      document.getElementById(resultName).value += item.value + "\n";
+      break;
     }
+  }
 }
 
 function choosedItemCheck(checkName, resultName) {
-    const arrItems = document.getElementsByName(checkName);
-    //console.log(arrItems);
-    const arrItemsChecked = [];
-    for (const item of arrItems) {
-        if (item.checked == true) {
-            //console.log(document.getElementsByName(checkName));
-            arrItemsChecked.push(item.value);
-        }
-        //return arrItemsChecked;
+  const arrItems = document.getElementsByName(checkName);
+  //console.log(arrItems);
+  const arrItemsChecked = [];
+  for (const item of arrItems) {
+    if (item.checked == true) {
+      //console.log(document.getElementsByName(checkName));
+      arrItemsChecked.push(item.value);
     }
-    console.log(arrItemsChecked);
-    document.getElementById(checkName).value = "";
-    document.getElementById(resultName).value = "";
-    arrItemsChecked.forEach(itemChecked => {
-        document.getElementById(checkName).value += itemChecked + ',';
-    });
-    document.getElementById(resultName).value += arrItemsChecked.join(", ") + '\n';   // to "#userChoice"
+    //return arrItemsChecked;
+  }
+  console.log(arrItemsChecked);
+  document.getElementById(checkName).value = "";
+  document.getElementById(resultName).value = "";
+  arrItemsChecked.forEach((itemChecked) => {
+    document.getElementById(checkName).value += itemChecked + ",";
+  });
+  document.getElementById(resultName).value +=
+    arrItemsChecked.join(", ") + "\n"; // to "#userChoice"
 }
 
 function choosedModeItemRadio(radioName, resultName) {
-    const arrItems = document.getElementsByName(radioName);
-    for (const item of arrItems) {
-        if (item.checked == true) {
-            //document.getElementById(radioName).value = item.value;
-            document.getElementById(resultName).value = '';
-            document.getElementById(resultName).value += item.value + '\n';
-            break;
-        }
+  const arrItems = document.getElementsByName(radioName);
+  for (const item of arrItems) {
+    if (item.checked == true) {
+      //document.getElementById(radioName).value = item.value;
+      document.getElementById(resultName).value = "";
+      document.getElementById(resultName).value += item.value + "\n";
+      break;
     }
+  }
 }
 
-function submitTaskSettings() { 
-    //arrCompLangs = str.split(',');
-    let strPrimLang = document.getElementById("resultPrim").value;
-    console.log(strPrimLang);
-    localStorage.setItem('primLang',strPrimLang);
+function submitTaskSettings() {
+  //arrCompLangs = str.split(',');
+  let strPrimLang = document.getElementById("resultPrim").value;
+  console.log(strPrimLang);
+  localStorage.setItem("primLang", strPrimLang);
 
-    let strCompLangsN = document.getElementById("resultComp").value;
-    let strCompLangs = strCompLangsN.replace(/\n/g, '')
-    console.log(strCompLangs);
-    localStorage.setItem('compLangs',strCompLangs);
-    //localStorage.removeItem('compLamgs');
+  let strCompLangsN = document.getElementById("resultComp").value;
+  let strCompLangs = strCompLangsN.replace(/\n/g, "");
+  console.log(strCompLangs);
+  localStorage.setItem("compLangs", strCompLangs);
+  //localStorage.removeItem('compLamgs');
 
-    let strThemesN = document.getElementById("resultThemes").value;
-    let strThemes = strThemesN.replace(/\n/g, '');
-    console.log(strThemes);
-    localStorage.setItem('themesLangs', strThemes);
+  let strThemesN = document.getElementById("resultThemes").value;
+  let strThemes = strThemesN.replace(/\n/g, "");
+  console.log(strThemes);
+  localStorage.setItem("themesLangs", strThemes);
 
-    let strModeChoice = document.getElementById('resultMode').value;
-    console.log(strModeChoice);
-    localStorage.setItem('modeChoice', strModeChoice);
+  let strModeChoice = document.getElementById("resultMode").value;
+  console.log(strModeChoice);
+  localStorage.setItem("modeChoice", strModeChoice);
 
-    //window.location.href='../pages/home.html';
-    getTaskPage();
-
+  //window.location.href='../pages/home.html';
+  getTaskPage();
 }
 
 // function getTaskPage() {
 //     let modeChoice = resultMode.value.trim();
 //     console.log(modeChoice);
-    
+
 //     if (modeChoice == "Plain") {
 //         window.location.href = "../pages/hometaskPlain.html";
 //     } else if (modeChoice == "Choice") {
@@ -256,7 +256,6 @@ function submitTaskSettings() {
 //         window.location.href = "../pages/taskMix.html";
 //     } else {
 //         window.location.href = "../pages/home.html";
-//     }          
-        
-// }
+//     }
 
+// }
