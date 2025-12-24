477e05d51572b9e7154903b9560bb6bdf5c9cf14
Elena-admin <avb.moons@gmail.com>
2025-12-24 13:29:47 +0300

    add tutorial

--- scripts/homeLoading.js
+++ scripts/homeLoading.js
@@ -1,98 +1,122 @@
-const viewportWidth = window.innerWidth + "px";
-console.log("ViewportWidth = ", viewportWidth);
+const viewportWidth = window.innerWidth + 'px';
+console.log('ViewportWidth = ', viewportWidth);
 
 const root = document.documentElement;
 root.style.setProperty('--viewport-width', viewportWidth);
 
 const pageUrl = window.location.pathname;
-let pageName = pageUrl.substring(pageUrl.lastIndexOf('/')+1);
+let pageName = pageUrl.substring(pageUrl.lastIndexOf('/') + 1);
 console.log(pageName);
 
 //// Modal Results open-close
 
-const resultsButton = document.getElementById("resultsButton");
-const resultsModal = document.getElementById("resultsModal");
-const redultsClose = document.getElementById("resultsClose");
+const resultsButton = document.getElementById('resultsButton');
+const resultsModal = document.getElementById('resultsModal');
+const redultsClose = document.getElementById('resultsClose');
 
 const openModalResults = () => {
-    resultsModal.style.display = "flex";
-}
+  resultsModal.style.display = 'flex';
+};
 
 const closeModalResults = () => {
-    resultsModal.style.display = "none";
-}
+  resultsModal.style.display = 'none';
+};
 
 //// Results totals
 
-let enjoyChoice = document.getElementById("enjoyChoice");
-let worryChoice = document.getElementById("worryChoice");
+let enjoyChoice = document.getElementById('enjoyChoice');
+let worryChoice = document.getElementById('worryChoice');
 
-let enjoyMix = document.getElementById("enjoyMix");
-let worryMix = document.getElementById("worryMix");
+let enjoyMix = document.getElementById('enjoyMix');
+let worryMix = document.getElementById('worryMix');
 
-let enjoyLang = document.getElementById("enjoyLang");
-let worryLang = document.getElementById("worryLang");
+let enjoyLang = document.getElementById('enjoyLang');
+let worryLang = document.getElementById('worryLang');
 
-let enjoyGrand = document.getElementById("enjoyGrand");
-let worryGrand = document.getElementById("worryGrand");
+let enjoyGrand = document.getElementById('enjoyGrand');
+let worryGrand = document.getElementById('worryGrand');
 
 function grandTotals() {
-    let totalsChoice = localStorage.getItem('totalsChoice');
-    let strTotalsChoice = totalsChoice.split(',');
-    let numTotalsChoice = strTotalsChoice.map(item => {
-        return parseInt(item.trim());
-    });//
-    console.log("Totals Choice: ");
-    console.log(numTotalsChoice);
-
-    let totalsMix = localStorage.getItem('totalsMix');
-    let strTotalsMix = totalsMix.split(',');
-    let numTotalsMix = strTotalsMix.map(item => {
-        return parseInt(item.trim());
+  let totalsChoice = localStorage.getItem('totalsChoice');
+  let strTotalsChoice;
+  let numTotalsChoice;
+  if (totalsChoice == null) {
+    strTotalsChoice == '0, 0';
+    numTotalsChoice == [0, 0]; //
+  } else {
+    strTotalsChoice = totalsChoice.split(',');
+    numTotalsChoice = strTotalsChoice.map((item) => {
+      return parseInt(item.trim());
     });
-    console.log("Totals Mix: ");
-    console.log(numTotalsMix);
+  }
 
-    let totalsLang = localStorage.getItem('totalsLang');
-    let strTotalsLang = totalsLang.split(',');
-    let numTotalsLang = strTotalsLang.map(item => {
-        return parseInt(item.trim());
+  console.log('Totals Choice: ');
+  console.log(numTotalsChoice);
+
+  let totalsMix = localStorage.getItem('totalsMix');
+  let strTotalsMix;
+  let numTotalsMix;
+  if (totalsMix == null) {
+    strTotalsMix = '0, 0';
+    numTotalsMix = [0, 0];
+  } else {
+    strTotalsMix = totalsMix.split(',');
+    numTotalsMix = strTotalsMix.map((item) => {
+      return parseInt(item.trim());
     });
-    console.log("Totals Lang: ");
-    console.log(numTotalsLang);
+  }
 
-    //// Results modal and report
+  console.log('Totals Mix: ');
+  console.log(numTotalsMix);
 
-    enjoyChoice.textContent = numTotalsChoice[0];
-    worryChoice.textContent = numTotalsChoice[1];
+  let totalsLang = localStorage.getItem('totalsLang');
+  let strTotalsLang;
+  let numTotalsLang;
+  if (totalsLang == null) {
+    strTotalsLang == '0, 0';
+    numTotalsLang == [0, 0];
+  } else {
+    strTotalsLang = totalsLang.split(',');
+    numTotalsLang = strTotalsLang.map((item) => {
+      return parseInt(item.trim());
+    });
+  }
 
-    enjoyMix.textContent = numTotalsMix[0];
-    worryMix.textContent = numTotalsMix[1];
+  console.log('Totals Lang: ');
+  console.log(numTotalsLang);
 
-    enjoyLang.textContent = numTotalsLang[0];
-    worryLang.textContent = numTotalsLang[1];
+  //// Results modal and report
 
-    let grandTotalEnjoy = numTotalsChoice[0] + numTotalsMix[0] + numTotalsLang[0];
-    let grandTotalWorry = numTotalsChoice[1] + numTotalsMix[1] + numTotalsLang[1];
-    let grandTotal = grandTotalEnjoy + grandTotalWorry;       
+  enjoyChoice.textContent = numTotalsChoice[0];
+  worryChoice.textContent = numTotalsChoice[1];
 
-    if (pageName = "home.html") {
-        let grandEnjoy = document.getElementById("grandEnjoy");
-        let grandWorry = document.getElementById("grandWorry");
+  enjoyMix.textContent = numTotalsMix[0];
+  worryMix.textContent = numTotalsMix[1];
 
-        if (grandTotal === 0 || grandTotal === null) {
-            grandTotal = 1;
-        }
+  enjoyLang.textContent = numTotalsLang[0];
+  worryLang.textContent = numTotalsLang[1];
 
-        let valueGrandEnjoy = Math.round(grandTotalEnjoy * 100 / grandTotal);
-        let valueGrandWorry = Math.round(grandTotalWorry * 100 / grandTotal);
+  let grandTotalEnjoy = numTotalsChoice[0] + numTotalsMix[0] + numTotalsLang[0];
+  let grandTotalWorry = numTotalsChoice[1] + numTotalsMix[1] + numTotalsLang[1];
+  let grandTotal = grandTotalEnjoy + grandTotalWorry;
 
-        enjoyGrand.textContent = valueGrandEnjoy + "%";
-        worryGrand.textContent = valueGrandWorry + "%"; 
+  if ((pageName = 'home.html')) {
+    let grandEnjoy = document.getElementById('grandEnjoy');
+    let grandWorry = document.getElementById('grandWorry');
 
-        grandEnjoy.textContent = valueGrandEnjoy + "%";
-        grandWorry.textContent = valueGrandWorry + "%";
-    } 
+    if (grandTotal === 0 || grandTotal === null) {
+      grandTotal = 1;
+    }
+
+    let valueGrandEnjoy = Math.round((grandTotalEnjoy * 100) / grandTotal);
+    let valueGrandWorry = Math.round((grandTotalWorry * 100) / grandTotal);
+
+    enjoyGrand.textContent = valueGrandEnjoy + '%';
+    worryGrand.textContent = valueGrandWorry + '%';
+
+    grandEnjoy.textContent = valueGrandEnjoy + '%';
+    grandWorry.textContent = valueGrandWorry + '%';
+  }
 }
 
 grandTotals();
@@ -115,5 +139,3 @@ grandTotals();
 //     doc.save("test-pdf.pdf");
 
 // })
-
-
---
+++ scripts/tutor.js
@@ -0,0 +1,37 @@
+const goButton = document.getElementById('goButton');
+const tutorCover = document.getElementById('tutorCover');
+const indexCover = document.getElementById('indexCover');
+
+const slider = document.querySelectorAll('.tutor-main');
+
+let slideIndex = 0;
+//showSlides();
+
+const openTutor = () => {
+  indexCover.style.display = 'none';
+  tutorCover.style.display = 'flex';
+  showSlides();
+};
+
+function showSlides() {
+  let slider = document.querySelectorAll('.tutor-main');
+  let tutorCover = document.getElementById('tutorCover');
+  let tutorCoverStyle = window.getComputedStyle(tutorCover);
+  let tutorCoverDisplay = tutorCoverStyle.display;
+  //console.log(tutorCoverDisplay);
+
+  for (let i = 0; i < slider.length; i++) {
+    slider[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slider.length) {
+    //slideIndex = 1;
+    clearInterval();
+    window.location.href = 'pages/home.html';
+  }
+  slider[slideIndex - 1].style.display = 'flex';
+  if (tutorCoverDisplay == 'flex') {
+    setTimeout(showSlides, 5000); //
+  }
+  //setTimeout(showSlides, 5000);
+}
